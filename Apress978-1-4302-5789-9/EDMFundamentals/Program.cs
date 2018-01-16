203186f47aba7300f041a02e89ce06c47fde9391
blackheart100 <1290823140@qq.com>
2018-01-16 09:53:51 +0800

    17

---
+++ Apress978-1-4302-5789-9/EDMFundamentals/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDMFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunCollection.InsertingAndRetrieving();
+            Console.ReadKey();
+        }
+    }
+}
