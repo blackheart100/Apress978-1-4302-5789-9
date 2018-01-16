b06570da1c7c62e0c26ee8c0396aabb225443962
blackheart100 <1290823140@qq.com>
2018-01-16 17:53:11 +0800

    55

---
+++ Apress978-1-4302-5789-9/QueryEntityDataModel/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QueryEntityDataModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
